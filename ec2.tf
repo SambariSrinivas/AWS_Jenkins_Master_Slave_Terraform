5638cee73b05017f29cef65e9dcaea4910e67b6b
srinivassambari <samba.s999@gmail.com>
2022-02-20 16:27:58 +0100

    Chaning the key to key1 and installing docker and git in slave machine

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "jenkins-master" {
   ami             = "ami-033b95fb8079dc481"
   instance_type   = "t3.medium"
   security_groups = [aws_security_group.jenkins-sg.name]
-  key_name        = "Srini-key"
+  key_name        = "key1"
   user_data       = file("jenkins_master_install.sh")
   availability_zone = "us-east-1a"
   tags = {
@@ -14,7 +14,7 @@ resource "aws_instance" "jenkins-slave" {
   ami             = "ami-033b95fb8079dc481"
   instance_type   = "t3.medium"
   security_groups = [aws_security_group.jenkins-sg.name]
-  key_name        = "Srini-key"
+  key_name        = "key1"
   user_data       = file("jenkins_slave_install.sh")
   availability_zone = "us-east-1a"
   tags = {
--- jenkins_slave_install.sh
+++ jenkins_slave_install.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 sudo amazon-linux-extras install epel -y
 sudo amazon-linux-extras install java-openjdk11
+sudo yum install git -y
+sudo yum install docker -y
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo systemctl status docker.service
