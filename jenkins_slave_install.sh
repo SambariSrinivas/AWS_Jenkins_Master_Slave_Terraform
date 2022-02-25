d572a0f4d9113f0cbde81f8b5518ddfe32081c82
srinivassambari <samba.s999@gmail.com>
2022-02-26 00:21:02 +0100

    Added Node.js support in salve machine

--- jenkins_slave_install.sh
+++ jenkins_slave_install.sh
@@ -6,3 +6,6 @@ sudo yum install docker -y
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo systemctl status docker.service
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+sudo . ~/.nvm/nvm.sh
+sudo nvm install node
